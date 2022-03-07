36fcbe3454e3513e5c8312ce2949084628e6cc9d
PortalShooter <iliusha_1999nooh@mail.ru>
2022-03-07 20:59:53 +0300

    task 1 ready

--- 1.2/task-1.js
+++ 1.2/task-1.js
@@ -1,27 +1,52 @@
 #!/usr/bin/env node
 const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+const { hideBin } = require('yargs/helpers');
+const { log } = require('console');
+const argv = yargs(hideBin(process.argv))
+  .alias({
+    'year': 'y',
+    'month': 'm',
+    'date': 'd'
+  })
+  .argv
 
-if (argv.ships > 3 && argv.distance < 53.5) {
-  console.log('Plunder more riffiwobbles!')
-} else {
-  console.log('Retreat from the xupptumblers!')
-  console.log(argv.ships)
+const monthArr = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь',]
+
+const command = argv._[0]
+
+if(command === 'cmd') {
+  const action = argv._[1]
+  const date = new Date()
+  const params = process.argv.slice(2).length > 2
+  if(action === 'current') {
+    if(!params) {
+      console.log(date)
+    } else {
+      if(argv.year) {
+        console.log('Текущий год:', date.getFullYear())
+      } else if(argv.month) {
+        console.log('Текущий месяц:', monthArr[date.getMonth()] + `(${date.getMonth() + 1})`)
+      } else if(argv.date) {
+        console.log('Текущий день:', date.getDate)
+      }
+    }
+  } else if(action === 'add' && params) {
+    if(argv.year) {
+      date.setFullYear(date.getFullYear() + argv.year)
+    } else if(argv.month) {
+      date.setMonth(date.getMonth() + argv.month)
+    } else if(argv.date) {
+      date.setDate(date.getDate() + argv.date)
+    }
+    console.log(date)
+  } else if(action === 'sub' && params) {
+    if(argv.year) {
+      date.setFullYear(date.getFullYear() - argv.year)
+    } else if(argv.month) {
+      date.setMonth(date.getMonth() - argv.month)
+    } else if(argv.date) {
+      date.setDate(date.getDate() - argv.date)
+    }
+    console.log(date)
+  }
 }
-
-// console.log('argv', argv);
-
-// const command = process.argv.slice(2)
-
-// if(command[0] === '-year' || command[1] === '--year') {
-//   console.log(new Date().getFullYear)
-// }
-// console.log(command);
-// console.log(command[0], command[1])
-
-
-
-// const sum = nums.reduce((acc, cur) => acc + cur, 0)
-
-// console.log('Сумма', sum);
