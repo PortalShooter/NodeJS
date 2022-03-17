469afd0083f57abb253646a1e239fc83644cb7f0
ilay <iliuhsa_1999nooh@mail.ru>
2022-03-17 12:54:04 +0300

    task-2

--- 1.4/task-2.js
+++ 1.4/task-2.js
@@ -1,1 +1,33 @@
 #!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs/promises');
+fs.readdir(__dirname).then((dir) => {
+    dir.forEach(item => {
+        const extension = path.extname(item)
+        if(extension === '.json') {
+            fs.readFile(item, 'utf-8').then((file) => {
+                const parseJson = JSON.parse(file)
+                const percent = Math.round(parseJson.winGame / (parseJson.numberGame / 100 )) + '%';
+                const percentEagle = Math.round(parseJson.numberEagle / (parseJson.numberGame / 100 )) + '%';
+                const percentTails = Math.round(parseJson.numberTails / (parseJson.numberGame / 100 )) + '%';
+
+                console.log('Общее количество партий:', parseJson.numberGame)
+                console.log('Количество выигранных партий:', parseJson.winGame);
+                console.log('Количество проигранных партий:', parseJson.loseGame);
+                console.log('Процентное соотношение выигранных партий:', percent);
+                console.log('Частота выпадения орла:', percentEagle);
+                console.log('Частота выпадения решки:', percentTails);
+            })
+        }
+    })
+})
+
+
+
+// console.log('Общее количество партий:')
+// console.log('Количество выигранных партий:');
+// console.log('Количество проигранных партий:');
+// console.log('Процентное соотношение выигранных партий.');
+// console.log('Частота выпадения орла:');
+// console.log('Частота выпадения решки:');
