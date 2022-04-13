7a7734c2e742bf16e57c6bb440428c21b1b1d357
ilay <iliuhsa_1999nooh@mail.ru>
2022-04-13 13:21:02 +0300

    add counterApp

---
+++ counterApp/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+
+app.post('/counter/:bookId/incr', (req, res) => {
+    console.log(123);
+})
+
+app.get('/counter/:bookId', (req, res) => {
+    console.log(req.params);
+    console.log(222);
+})
+app.get('/counter', (req, res) => {
+    console.log(321);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`=== start server PORT ${PORT} ===`);
+});
