d4105e83fe6563830e5440b8efa26ebfda018384
PortalShooter <iliusha_1999nooh@mail.ru>
2022-04-27 22:29:01 +0300

    Finish

--- booksApp/index.js
+++ booksApp/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const userRoutes = require('./routes/user');
 const booksRoutesAPI = require('./routes/api/books');
 const booksRoutes = require('./routes/books');
-
-const PORT = process.env.PORT || 3000;
 
 const app = express()
 const bodyParser = require('body-parser')
@@ -21,6 +21,29 @@ app.get('/', (req, res) => {
 
 app.set('view engine', 'ejs');
 
-app.listen(PORT, () => {
-    console.log(`=== start server PORT ${PORT} ===`);
-});
+const PORT = process.env.PORT || 3000;
+const UserDB = process.env.DB_USERNAME || 'root';
+const PasswordDB = process.env.DB_PASSWORD || 'qwerty12345';
+const NameDB = process.env.DB_NAME || 'books';
+const HostDb = process.env.DB_HOST || 'mongodb://localhost:27017/';
+
+(async () => {
+
+    mongoose.connect(HostDb, {
+        user: UserDB,
+        pass: PasswordDB,
+        dbName: NameDB,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        app.listen(PORT, () => {
+            console.log(`=== start server PORT ${PORT} ===`);
+        });
+    })
+    .catch ((e) => {
+        console.log('Error', e);
+    })
+
+})();
+
+
--- booksApp/routes/books.js
+++ booksApp/routes/books.js
@@ -1,39 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book');
 
-const fetch = require('node-fetch');
-
-async function getViewBook(bookId) {
-    const response = await fetch(`http://counter:3002/counter/${bookId}`);
-    const data = await response.json();
-    console.log('getViewBook', data);
-    return data.count
-}
-
-async function postViewBook(bookId) {
-    const response = await fetch(`http://counter:3002/counter/${bookId}/incr`,  {method: 'POST'});
-    const data = await response.json();
-
-    console.log('postViewBook', data);
-}
-
-const library = [
-    {
-        "title": "Мартин Иден",
-        "description": "Юный моряк и выходец из низов Мартин Иден случайно знакомится с девушкой из состоятельной семьи Руфью Морз. Влюбившись с первого взгляда и попав под обаяние высшего света, Мартин решает стать достойным своей избранницы. Отныне его жизнь будет посвящена тому, чтобы реализовать пробудившийся писательский дар, добиться славы и руки любимой женщины",
-        "authors": "Джек Лондон",
-        "favorite": "string",
-        "fileCover": "string",
-        "fileName": "martin_iden.epub2022-03-31T13:08:36.212Z",
-        "fileBook": "public/books/martin_iden.epub2022-03-31T13:08:36.212Z",
-        "id": "20220327"
-    },
-];
-
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const books = await Book.find();
     res.render('books/index', {
         title: 'Список книг',
-        bookList: library,
+        bookList: books,
     })
 })
 
@@ -43,80 +16,88 @@ router.get('/create', (req, res) => {
     })
 })
 
-router.post('/create', (req, res) => {
-    const {title, desc, author} = req.body
-    library.push(
-        {
-            "title": title,
-            "description": desc,
-            "authors":author ,
-            "fileName": "martin_iden.epub2022-03-31T13:08:36.212Z",
-            "fileBook": "public/books/martin_iden.epub2022-03-31T13:08:36.212Z",
-            "id": new Date().toISOString().slice(0,15).replace(/-/g,'')
-        }
-    )
-    res.redirect('/books')
-})
+router.post('/create', async (req, res) => {
+    const {title, description, authors} = req.body
 
-router.post('/update/:id', (req, res) => {
-    const {id} = req.params
-    const idx = library.findIndex(el => el.id === id);
+    const newBook = new Book({
+        title, authors, description, id: new Date().toISOString().slice(0,19).replace(/-/g,'')
+    });
 
-    if(idx === -1) {
-        res.render('error/404', {
-            title: '404',
-            description: 'Похоже такой книги у нас нет)',
-        })
-    } else {
-        const {title, author, desc} = req.body
-        library[idx].title = title
-        library[idx].authors = author
-        library[idx].description = desc
+    try {
+        await newBook.save();
         res.redirect('/books')
+    } catch (e) {
+        console.error(e);
     }
+
 })
 
-router.get('/update/:id', (req, res) => {
+router.post('/update/:id', async (req, res) => {
     const {id} = req.params
-    const idx = library.findIndex(el => el.id === id);
+    const {title, authors, description} = req.body
 
-    if(idx === -1) {
+    try {
+        await Book.findOneAndUpdate({id}, {title, authors, description});
+         res.redirect('/books')
+    } catch (e) {
+        console.error(e);
         res.render('error/404', {
             title: '404',
             description: 'Похоже такой книги у нас нет)',
         })
-    } else {
+    }
+})
+
+router.get('/update/:id', async (req, res) => {
+    const {id} = req.params
+    const book = await Book.findOne({ id })
+
+    if(book) {
         res.render('books/update', {
-            title: library[idx].title,
-            book: library[idx],
+            title: book.title,
+            book: book,
         })
-    }
-    
-})
-
-router.get('/:id', (req, res) => {
-    const {id} = req.params
-    const idx = library.findIndex(el => el.id === id);
-
-    if(idx === -1) {
+    } else {
         res.render('error/404', {
             title: '404',
             description: 'Похоже такой книги у нас нет)',
         })
-    } else {
-
-        (async () => {
-            postViewBook(id)
-            const viewBook = await getViewBook(id)
+    }
     
-            res.render('books/view', {
-                title: library[idx].title,
-                book: library[idx],
-                viewBook: viewBook,
-            })
-        })();
-       
+})
+
+router.get('/:id', async (req, res) => {
+
+    const {id} = req.params
+    const book = await Book.findOne({ id })
+
+    if(book) {
+        res.render('books/view', {
+            title: book.title,
+            book: book,
+        })
+        
+    } else {
+        res.render('error/404', {
+            title: '404',
+            description: 'Похоже такой книги у нас нет)',
+        })
     }
 })
+
+router.post('/delete/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        await Book.findOneAndDelete({ id });
+        res.redirect('/books')
+    } catch (e) {
+        console.error(e);
+        res.render('error/404', {
+            title: '404',
+            description: 'Похоже такой книги у нас нет)',
+        })
+    }
+});
 
 module.exports = router;
